e8557a5a555980b32225d464b43ece12b294cf9e
yusuf emad <yusufy2004@gmail.com>
2023-10-30 12:07:16 +0200

    edits for api gateway

--- lambda_handler.py
+++ lambda_handler.py
@@ -3,22 +3,23 @@ import json
 
 
 def lambda_handler(event, context):
-    body = json.loads(event["body"])
-    question = body["question"]
-    session_id = body["session_id"]
+    if event["requestContext"]["http"]["method"] == "POST":
+        body = json.loads(event["body"])
+        question = body["question"]
+        session_id = body["session_id"]
 
-    agent = get_agent(Agent.TEST, session_id)
+        agent = get_agent(Agent.TEST, session_id)
 
-    try:
-        response = agent(question)["output"]
-    except Exception as e:
-        # return an error with status code suitable for the error
+        try:
+            response = agent(question)["output"]
+        except Exception as e:
+            # return an error with status code suitable for the error
+            return {
+                "statusCode": 500,
+                "body": json.dumps({"error": str(e)}),
+            }
+
         return {
-            "statusCode": 500,
-            "body": json.dumps({"error": str(e)}),
+            "statusCode": 200,
+            "body": json.dumps({"answer": response}),
         }
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"answer": response}),
-    }
