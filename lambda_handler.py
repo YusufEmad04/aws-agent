5dd0df00e6179213d7b27930804ce59ecfeff2a5
yusuf emad <yusufy2004@gmail.com>
2023-10-30 17:00:13 +0200

    revert back to normal lambda

--- lambda_handler.py
+++ lambda_handler.py
@@ -3,23 +3,23 @@ import json
 
 
 def lambda_handler(event, context):
-    if event["requestContext"]["http"]["method"] == "POST":
-        body = json.loads(event["body"])
-        question = body["question"]
-        session_id = body["session_id"]
+    # if event["requestContext"]["http"]["method"] == "POST":
+    body = json.loads(event["body"])
+    question = body["question"]
+    session_id = body["session_id"]
 
-        agent = get_agent(Agent.TEST, session_id)
+    agent = get_agent(Agent.TEST, session_id)
 
-        try:
-            response = agent(question)["output"]
-        except Exception as e:
-            # return an error with status code suitable for the error
-            return {
-                "statusCode": 500,
-                "body": json.dumps({"error": str(e)}),
-            }
-
+    try:
+        response = agent(question)["output"]
+    except Exception as e:
+        # return an error with status code suitable for the error
         return {
-            "statusCode": 200,
-            "body": json.dumps({"answer": response}),
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)}),
         }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"answer": response}),
+    }
